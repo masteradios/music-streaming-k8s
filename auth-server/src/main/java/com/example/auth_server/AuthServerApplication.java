8ec713683cab77d2e5284615acc3eef319356dc2
aditya <reachadikush@gmail.com>
2026-02-14 16:26:54 +0530

    Made the app docker suitable

--- auth-server/src/main/java/com/example/auth_server/AuthServerApplication.java
+++ auth-server/src/main/java/com/example/auth_server/AuthServerApplication.java
@@ -2,10 +2,8 @@ package com.example.auth_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
